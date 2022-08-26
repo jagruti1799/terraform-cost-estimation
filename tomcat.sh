e0b1485ce974b918d4e1a4fffb9e3458eb1fb024
jagruti1799 <110963747+jagruti1799@users.noreply.github.com>
2022-08-26 12:14:29 +0530

    update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -18,5 +18,12 @@ echo "Add following lines"
 printf "\n[Unit]\nDescription=Apache Tomcat Web Application Container\nAfter=network.target\n[Service]\nType=forking\nEnvironment=JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64/\nEnvironment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid\nEnvironment=CATALINA_HOME=/opt/tomcat\nEnvironment=CATALINA_BASE=/opt/tomcat\nEnvironment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'\nEnvironment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'\nExecStart=\nExecStart=/opt/tomcat/bin/startup.sh\nExecStop=/opt/tomcat/bin/shutdown.sh\nUser=tomcat\nGroup=tomcat\nUMask=0007\nRestartSec=10\nRestart=always\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/tomcat.service
 echo "Start tomcat"
 sudo systemctl start tomcat.service
-echo "Check status of tomcat"
-systemctl status tomcat.service
+echo "Install maven"
+sudo apt install maven -y
+mvn -version
+echo "clone github repo in machine"
+git clone https://github.com/jagruti1799/Tomcat-jakartaee.git
+cd Tomcat-jakartaee
+ls
+mvn clean
+sudo cp /home/einfochips/tomcat10-jakartaee9/target/helloworld.war /opt/tomcat/webapps
