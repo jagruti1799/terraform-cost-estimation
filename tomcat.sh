05e16600061292069c763d0e6e1ee80b5e9573c1
jagruti1799 <110963747+jagruti1799@users.noreply.github.com>
2022-08-31 16:02:35 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -2,7 +2,3 @@ echo "Create tomcat user and group"
 sudo useradd -m -d /opt/tomcat -U -s /bin/false tomcat
 echo "Update system"
 sudo apt update
-echo "Install java"
-sudo apt-get install openjdk-11-jdk -y
-echo "check java version"
-java -version
