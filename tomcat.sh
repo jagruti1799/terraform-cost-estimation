f3f16f8d44503a2ac1e243e32a73e2b90d17e057
jagruti1799 <110963747+jagruti1799@users.noreply.github.com>
2022-08-31 15:56:18 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -6,24 +6,3 @@ echo "Install java"
 sudo apt-get install openjdk-11-jdk -y
 echo "check java version"
 java -version
-echo "Download tar file for tomcat"
-wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.23/bin/apache-tomcat-10.0.23.tar.gz
-echo "unzip tar file"
-sudo tar xzvf apache-tomcat-10*tar.gz -C /opt/tomcat --strip-components=1
-echo "Change ownership"
-sudo chown -R tomcat:tomcat /opt/tomcat/
-echo "Change file permission"
-sudo chmod -R u+x /opt/tomcat/bin
-echo "Add following lines"
-printf "\n[Unit]\nDescription=Apache Tomcat Web Application Container\nAfter=network.target\n[Service]\nType=forking\nEnvironment=JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64/\nEnvironment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid\nEnvironment=CATALINA_HOME=/opt/tomcat\nEnvironment=CATALINA_BASE=/opt/tomcat\nEnvironment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'\nEnvironment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'\nExecStart=\nExecStart=/opt/tomcat/bin/startup.sh\nExecStop=/opt/tomcat/bin/shutdown.sh\nUser=tomcat\nGroup=tomcat\nUMask=0007\nRestartSec=10\nRestart=always\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/tomcat.service
-echo "Start tomcat"
-sudo systemctl start tomcat.service
-echo "Install maven"
-sudo apt install maven -y
-mvn -version
-echo "clone github repo in machine"
-git clone https://github.com/jagruti1799/Tomcat-jakartaee.git
-cd Tomcat-jakartaee
-ls
-mvn clean
-sudo cp /home/einfochips/tomcat10-jakartaee9/target/helloworld.war /opt/tomcat/webapps
